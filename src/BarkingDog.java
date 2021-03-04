4226639c8dff7831116f51c072912d6d1042fdae
Jonathan <lacayo.j27@gmail.com>
2021-03-04 16:21:17 -0600

    Initial commit

---
+++ src/BarkingDog.java
@@ -0,0 +1,61 @@
+public class BarkingDog {
+
+
+//    We have a dog that likes to bark. We need to wake up
+//    if the dog barks at night!
+//
+//    Write a method shouldWakeUp that has 2 parameters
+//
+//    1st parameter should be of type boolean and be named barking
+//    it represents if our dog is currently barking.
+//
+//    2nd parameter represents the hour of the day and is of getType
+//    int with the name hourOfDay and has a valid range of 0-23
+//
+//    We have to wake up if the dog is barking before 8 or after 22 hours
+//    so in that case return true. In all others return false
+//
+//    If The hourOfDay parameter is less than 0 or greater than 23
+//    return false.
+//
+//    shouldWakeup(true,1);  Should return true
+//    shouldWakeup(false,2);  Should return false
+
+
+    public static void main(String[] args) {
+
+        System.out.println(shouldWakeUp(true, 1));
+        System.out.println(shouldWakeUp(false, 2));
+        System.out.println(shouldWakeUp(true, 8));
+        System.out.println(shouldWakeUp(true, -1));
+
+
+
+    }
+
+
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+
+        if (barking == false || (hourOfDay <= 0 || hourOfDay > 23)) {
+
+            return false;
+        } else {
+
+            if (hourOfDay < 8 || hourOfDay > 22) {
+
+                return true;
+
+
+            } else {
+
+
+                return false;
+            }
+
+        }
+
+
+    }
+
+
+}
